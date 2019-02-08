ef56df17a97b0b78ded777885b021c6d23dd007f
Ростислав Абалов <gasick@mail.ru>
2019-02-08 19:40:21 +0300

    Update

--- app/src/main/java/com/gcorp/knitshceme/Pattern.java
+++ app/src/main/java/com/gcorp/knitshceme/Pattern.java
@@ -156,20 +156,34 @@ public class Pattern {
     //возвращаем значение рисунка png из drawable
     public static int getRDrawablePNG(cell c) {
         switch (c) {
-            case p2tls: return R.drawable.p2tls;
-            case p2trs: return R.drawable.p2trs;
-            case k2tls: return R.drawable.k2tls;
-            case k2trs: return R.drawable.k2trs;
-            case p3tls: return R.drawable.p3tls;
-            case k3tls: return R.drawable.k3tls;
-            case purl: return R.drawable.purl;
-            case knit: return R.drawable.knit;
-            case yarnover: return R.drawable.yarnover;
-            case ils: return R.drawable.ils;
-            case irs: return R.drawable.irs;
-            case kitb: return R.drawable.kitb;
-            case pitb: return R.drawable.pitb;
-            default: return -1;
+            case p2tls:
+                return R.drawable.p2tls;
+            case p2trs:
+                return R.drawable.p2trs;
+            case k2tls:
+                return R.drawable.k2tls;
+            case k2trs:
+                return R.drawable.k2trs;
+            case p3tls:
+                return R.drawable.p3tls;
+            case k3tls:
+                return R.drawable.k3tls;
+            case purl:
+                return R.drawable.purl;
+            case knit:
+                return R.drawable.knit;
+            case yarnover:
+                return R.drawable.yarnover;
+            case ils:
+                return R.drawable.ils;
+            case irs:
+                return R.drawable.irs;
+            case kitb:
+                return R.drawable.kitb;
+            case pitb:
+                return R.drawable.pitb;
+            default:
+                return -1;
         }
     }
 
